9e50dd6a281e7b468ba98189b34a7d3eeed92669
Str-F <frederik_struemke@icloud.com>
2024-07-09 14:30:57 +0200

    performance and bug fixes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from timeit import timeit
+
 def analys_file(filename):
     with open(filename, "r") as f:
-        lines = f.readlines()
-    bagcount = int(lines[0])
-    gamescount = int(lines[1])
-    games = []
-    for i in range(2, gamescount+2):
-        games.append(int(lines[i]))
+        bagcount = int(f.readline())
+        gamescount = int(f.readline())
+        games = []
+        for i in range(2, gamescount+2):
+            games.append(int(f.readline()))
 
     return bagcount, gamescount, games
 
-bagcount, gamescount, games = analys_file("data/wundertuete0.txt")
+bagcount, gamescount, games = analys_file('data/wundertuete0.txt')
+print(timeit('analys_file("data/wundertuete0.txt")', number=50, globals={'analys_file': analys_file}))
 
 print(bagcount, gamescount, games)
